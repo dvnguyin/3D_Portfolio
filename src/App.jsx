b249e6f88992a35cf27f07c655b83b8650dd7653
dvnguyin <david.nguyen20x@gmail.com>
2025-06-16 22:50:28 -0400

    'Add Experience section with interactive glow effect and styling'

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import LogoSection from "./sections/LogoSection.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
 
 const App = () => {
     return (
 <>
     <NavBar />
-<Hero />
+    <Hero />
     <ShowcaseSection />
     {/* <LogoSection   /> */}
     <FeatureCards />
+    <ExperienceSection />
 </>
     )
 }
